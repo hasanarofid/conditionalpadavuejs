89221da74a1a4ad8f8a41ce23dbd991f678d6d38
hasanarofid <hasanarofid@gmail.com>
2018-09-05 09:48:57 +0700

    oke

---
+++ apps.js
@@ -0,0 +1,24 @@
+new Vue({
+  el: "#app",
+  data: {
+    tampilkan: "tampil",
+    message: "Hello Vue.js!"
+  }
+});
+
+new Vue({
+  el: "#app2",
+  data: {
+    showif: "ada",
+    datashow: "penerapan if show"
+  }
+});
+
+new Vue({
+  el: "#app3",
+  data: {
+    tampilif: false,
+    jikaif: "conditional pada vue js",
+    jikaelse: "conditional else pada vue js"
+  }
+});
